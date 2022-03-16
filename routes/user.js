cbecdedd59977b28939d8c3de9d4d7836a2ec198
gupta.tanmay01@gmail.com <gupta.tanmay01@gmail.com>
2022-03-16 22:09:45 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { deleteUser } = require('../controllers/user');
+const router = express.Router();
+const { isLoggedIn } = require('../middlewares');
+
+router.delete('/user/delete', isLoggedIn, deleteUser);
+
+module.exports = router;
